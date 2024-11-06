f88023957c0980f0f073992d38625386876c633e
Michael Gorbunov <mg0rby@yandex.ru>
2024-11-06 18:26:04 +0300

    список получателей и сообщений

--- mailing/urls.py
+++ mailing/urls.py
@@ -2,12 +2,22 @@
 from django.urls import path
 from mailing.apps import MailingConfig
 from mailing import views
-from .views import RecipientCreateView,RecipientUpdateView
+from .views import RecipientCreateView,RecipientUpdateView,RecipientListView
+from .views import MessageUpdateView,MessageCreateView,MessageListView
+from .views import MailingCreateView,MailingUpdateView
 app_name = MailingConfig.name
 
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('base/', views.base, name='base'),
     path('recipient_create/', RecipientCreateView.as_view(), name='recipient_create'),
-    path('recipient_update/<int:pk>', RecipientUpdateView.as_view(), name='recipient_updatee'),
+    path('recipient_list/', RecipientListView.as_view(), name='recipient_list'),
+    path('recipient_update/<int:pk>', RecipientUpdateView.as_view(), name='recipient_update'),
+
+    path('message_create/', MessageCreateView.as_view(), name='message_create'),
+    path('message_update/<int:pk>', MessageUpdateView.as_view(), name='message_update'),
+    path('message_list/',MessageListView.as_view(), name='message_list'),
+
+    path('mailing_create/', MailingCreateView.as_view(), name='mailing_create'),
+    path('mailing_update/<int:pk>', MailingUpdateView.as_view(), name='mailing_update'),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .forms import RecipientForm,MailingForm,MessageForm
-from django.views.generic.edit import CreateView,UpdateView
-from .models import Mailing,Message,Recipient
+from .forms import RecipientForm, MailingForm, MessageForm
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
+from django.views.generic import DetailView, ListView, TemplateView
+from .models import Mailing, Message, Recipient
+from django.urls import reverse, reverse_lazy
+
 
 # Create your views here.
 
@@ -9,20 +12,66 @@ from .models import Mailing,Message,Recipient
 def about(request):
     return render(request, 'mailing/about.html')
 
+
 def base(request):
     return render(request, 'mailing/base.html')
 
+
+class RecipientListView(ListView):
+    model = Recipient
+    template_name = "mailing/recipient_list.html"
+    context_object_name = "recipients"
+
+
 class RecipientCreateView(CreateView):
-    model =Recipient  # Указываем модель, с которой будет работать это представление
+    """view для создания получателя рассылки"""
+    model = Recipient  # Указываем модель, с которой будет работать это представление
     form_class = RecipientForm  # Указываем форму, которая будет использоваться для ввода данных
     template_name = 'mailing/recipient_form.html'  # Шаблон, который будет использоваться для отображения формы
-    success_url = '/base/'  # URL, на который будет перенаправлен пользователь после успешной отправки формы
+    success_url = reverse_lazy('mailing:recipient_list')  # URL, на который будет перенаправлен пользователь после успешной отправки формы
+
 
 class RecipientUpdateView(UpdateView):
-    model =Recipient  # Указываем модель, с которой будет работать это представление
+    """view для обновления получателя рассылки"""
+    model = Recipient  # Указываем модель, с которой будет работать это представление
     form_class = RecipientForm  # Указываем форму, которая будет использоваться для ввода данных
     template_name = 'mailing/recipient_form.html'  # Шаблон, который будет использоваться для отображения формы
+    success_url = reverse_lazy('mailing:recipient_list')  # URL, на который будет перенаправлен пользователь после успешной отправки формы
+
+
+class MessageListView(ListView):
+    model = Message
+    template_name = "mailing/message_list.html"
+    context_object_name = "messages"
+
+class MessageUpdateView(UpdateView):
+    """view для обновления сообщения"""
+    model = Message  # Указываем модель, с которой будет работать это представление
+    form_class = MessageForm  # Указываем форму, которая будет использоваться для ввода данных
+    template_name = 'mailing/message_form.html'  # Шаблон, который будет использоваться для отображения формы
+    success_url = reverse_lazy('mailing:message_list')  # URL, на который будет перенаправлен пользователь после успешной отправки формы
+
+
+class MessageCreateView(CreateView):
+    """view для создания сообщения"""
+    model = Message  # Указываем модель, с которой будет работать это представление
+    form_class = MessageForm  # Указываем форму, которая будет использоваться для ввода данных
+    template_name = 'mailing/message_form.html'  # Шаблон, который будет использоваться для отображения формы
+    success_url = reverse_lazy('mailing:message_list')  # URL, на который будет перенаправлен пользователь после успешной отправки формы
+
+
+class MailingCreateView(CreateView):
+    """view для создания рассылки"""
+    model = Mailing  # Указываем модель, с которой будет работать это представление
+    form_class = MailingForm  # Указываем форму, которая будет использоваться для ввода данных
+    template_name = 'mailing/mailing_form.html'  # Шаблон, который будет использоваться для отображения формы
     success_url = '/base/'  # URL, на который будет перенаправлен пользователь после успешной отправки формы
 
 
+class MailingUpdateView(UpdateView):
+    """view для создания рассылки"""
+    model = Mailing  # Указываем модель, с которой будет работать это представление
+    form_class = MailingForm  # Указываем форму, которая будет использоваться для ввода данных
+    template_name = 'mailing/mailing_form.html'  # Шаблон, который будет использоваться для отображения формы
+    success_url = '/base/'  # URL, на который будет перенаправлен пользователь после успешной отправки формы
 
